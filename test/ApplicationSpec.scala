d7f7bf95ea1329a0230d35ca53a35a5442acd255
Antonio Murgia <antonio.murgia@eng.it>
2017-03-18 18:09:30 +0100

    [#develop] Change the way travis runs tests

--- test/ApplicationSpec.scala
+++ test/ApplicationSpec.scala
@@ -1,13 +1,21 @@
+import dao.{CountriesDAO, InMemoryCountriesDAO}
 import org.scalatestplus.play._
+import parser.{CsvAirportParser, CsvCountryParser, CsvRunwayParser}
 import play.api.test._
 import play.api.test.Helpers._
-
+import scala.language.reflectiveCalls
 /**
  * Add your spec here.
  * You can mock out a whole application including requests, plugins etc.
  * For more information, consult the wiki.
  */
 class ApplicationSpec extends PlaySpec with OneAppPerTest {
+
+  val fixture = new {
+    import scala.concurrent.ExecutionContext.Implicits.global
+    val dao: CountriesDAO = new InMemoryCountriesDAO(new CsvAirportParser(), new CsvRunwayParser(), new CsvCountryParser())
+  }
+
 
   "Routes" should {
 
@@ -28,5 +36,21 @@ class ApplicationSpec extends PlaySpec with OneAppPerTest {
     }
 
   }
+
+
+//  GET        /v1/airports                 controllers.AirportsController.airportsForCountry(country: String)
+  "AirportsController" should {
+    "return all the airports of a given specific country" in {
+      val italianAirports = route(app, FakeRequest(GET, "/v1/airports?country=Italy")).get
+      status(italianAirports) mustBe OK
+    }
+  }
+
+//  GET        /v1/countries                controllers.CountriesController.countriesRankBy(query: String, order: String, count: Int ?= 10)
+
+//  GET        /v1/runways                  controllers.RunwaysController.runwaysRankBy(query: String, order: String, count: Int ?= 10)
+
+//  GET        /v1/countries/runways        controllers.CountriesController.countriesAndRunwayTypes()
+
 
 }
